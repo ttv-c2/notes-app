1bee0cc6bb7b8f7c7630eaa1e6e01d44c930714c
ttv-c2 <ttv-c2@tembotechventures.com>
2024-09-19 14:47:01 +0000

    add testing workflow

---
+++ src/components/custom-button/custom-button.test.tsx
@@ -0,0 +1,16 @@
+// custom-button.test.tsx
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import { CustomButton } from "./custom-button";
+
+describe("CustomButton", () => {
+  it("renders the button with the correct text", () => {
+    const document = render(<CustomButton />);
+
+    const buttonElement = document.getByRole("button", {
+      name: "CustomButton",
+    });
+
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
